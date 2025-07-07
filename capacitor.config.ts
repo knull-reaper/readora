34e25ef98cd0a41a95507dd5f61a1d26e6af904f
knull-reaper <godreaper3135@gmail.com>
2025-07-07 15:54:11 -0400

    for android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.readora.app',
+  appName: 'readora',
+  webDir: 'dist'
+};
+
+export default config;
