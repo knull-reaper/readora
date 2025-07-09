3fc4bf7ad5e972fbd2fc9157ee70a2602cc828c8
knull-reaper <godreaper3135@gmail.com>
2025-07-08 22:40:04 -0400

    first itteration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,27 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.readora.app',
-  appName: 'readora',
-  webDir: 'dist'
+  appId: "com.readora.app",
+  appName: "readora",
+  webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
